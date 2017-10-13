bd3020c7a056ca96efa613426145b9f2d42eb557
tzcccy <782635927@qq.com>
2017-10-13 19:11:55 +0800

    Initial Commit

---
+++ 单例和类别的设计/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  单例和类别的设计
+//
+//  Created by zx on 2017/10/13.
+//  Copyright © 2017年 maple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 单例和类别的设计/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  单例和类别的设计
+//
+//  Created by zx on 2017/10/13.
+//  Copyright © 2017年 maple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
